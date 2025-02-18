8fb2509b74f7e6542e4f436724ea8afd134b99ae
Mayank Deep <rishusinha525@gmail.com>
2025-02-19 02:53:40 +0530

    added concepts

---
+++ ESmodule/calculatorFunctionality.js
@@ -0,0 +1,20 @@
+
+export function add(a , b)
+{
+    return a + b;
+}
+
+export function substract(a , b)
+{
+    return a - b;
+}
+
+export function divide(a , b)
+{
+    return a / b;
+}
+
+export function multiplication(a , b)
+{
+    return a * b;
+}
---
+++ ESmodule/calculatorUser.js
@@ -0,0 +1,7 @@
+
+import { add , substract , divide , multiplication } from "./calculatorFunctionality.js";
+
+console.log(add(3 , 5));
+console.log(multiplication(2 , 3));
+console.log(substract(5 , 2));
+console.log(divide(10 , 5));
